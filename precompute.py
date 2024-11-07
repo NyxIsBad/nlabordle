b4e2c7b967fc2fb0d3a35baa04721e13b5cf1bfd
NyxIsBad <totallynotnyx@gmail.com>
2024-11-07 18:21:51 -0500

    now am correctly precomputing

--- precompute.py
+++ precompute.py
@@ -18,18 +18,22 @@ def resolveLink(data, link):
     return link
 
 # Implement computeCommonLinks
-def computeCommonLinks(data, a, b, aLinks):
+def computeCommonLinks(data, a, b):
     # Resolve the links
     a = resolveLink(data, a)
     b = resolveLink(data, b)
     # Get the resolved links for every single article on a and b
-    aLinks = set(map(lambda x: resolveLink(data, x), data[a].links))
-    bLinks = set(map(lambda x: resolveLink(data, x), data[b].links))
+    aLinks = set(map(lambda x: resolveLink(data, x), data[a].get('links', [])))
+    bLinks = set(map(lambda x: resolveLink(data, x), data[b].get('links', [])))
     for article in data:
+        article_links = data[article].get('links', [])
+        if not article_links:
+            continue
+        article_links = set(map(lambda x: resolveLink(data, x), article_links))
         # For each article, if a is in the links, add the article to aLinks
-        if data[article].links and resolveLink(data, a) in data[article].links:
+        if a in article_links:
             aLinks.add(article)
-        if data[article].links and resolveLink(data, b) in data[article].links:
+        if b in article_links:
             bLinks.add(article)
     # Return the length of the intersection of aLinks and bLinks
     return len(aLinks.intersection(bLinks))
@@ -45,15 +49,14 @@ def computeDistance(data, a, b):
     # BFS for the shortest path to b
     queue = [a]
     while True:
-        article = queue.pop(0)
-        # If we have gone through all the articles
-        if not article:
+        if len(queue) == 0:
             break
+        article = queue.pop(0)
         # If the article does not exist or has no links, skip
-        if not data[article] or not data[article].links:
+        if article not in data or not data[article].get('links', []):
             continue
         # Else resolve all links and add them to the queue
-        links = list(map(lambda x: resolveLink(data, x), data[article].links))
+        links = list(map(lambda x: resolveLink(data, x), data[article].get('links', [])))
         for link in links:
             if link in distances:
                 continue
@@ -62,7 +65,7 @@ def computeDistance(data, a, b):
             if link == b:
                 break
             queue.append(link)
-    return distances[b]
+    return distances[b] if b in distances else None
 
 # Implement random
 def random(n):
@@ -112,47 +115,30 @@ def load_data():
 
     return data, filtered_articles, difficulty, target
 
-def precompute(raw_data: dict, target: str):
+def precompute(raw_data: dict, filtered: list[str], target: str):
     # Essentially we want to precompute the common links and the distance between every pair of articles
     # We will store this in a dictionary
     # The dictionary will be stored as a binary file
     # We do this via a BFS traversal of the graph with the starting point as the target
     start = resolveLink(raw_data, target)
-    start_links = set(map(lambda x: resolveLink(raw_data, x), raw_data[start].get('links', [])))
-    for x in raw_data:
-        if raw_data[x].get('links') and start in raw_data[x]['links']:
-            start_links.add(x)
     data = {}
-    # Dict contains a tuple of (common links, distance)
-    data[start] = (0, 0)
-    queue = [start]
-    print(len(raw_data))
-    with tqdm(total=len(raw_data), desc="Precomputing") as pbar:
-        while True:
-            if len(queue) == 0:
-                break
-            article = queue.pop(0)
-            # If the article does not exist or has no links, skip
-            if not raw_data[article] or not raw_data[article].get('links'):
-                continue
-            # Else resolve all links and add them to the queue
-            links = list(map(lambda x: resolveLink(raw_data, x), raw_data[article].get('links', [])))
-            # For each link, compute the common links and distance
-            for link in links:
-                if not link in raw_data or link in data:
-                    continue
-                # print(f"Computing {article} -> {link}")
-                link = resolveLink(raw_data, link)
-                if link in data:
-                    continue
-                link_links = set(map(lambda x: resolveLink(raw_data, x), raw_data[link].get('links', [])))
-                for x in raw_data:
-                    if raw_data[x].get('links') and link in raw_data[x]['links']:
-                        link_links.add(x)
-                common_links = len(start_links.intersection(link_links))
-                data[link] = (common_links, data[article][1] + 1)
-                queue.append(link)
-            pbar.update(1)
+    # Dict contains a tuple of (avg_distance,, commonlinks, occurrences)
+    data[start] = (0, 0, 0)
+    for article in tqdm(filtered[:10], desc="Precomputing"):
+        if article == start:
+            continue
+        article_link = resolveLink(raw_data, article)
+        # We compute the distance from start to article and article to start
+        dists = [x for x in [computeDistance(raw_data, start, article_link), computeDistance(raw_data, article_link, start)] if x is not None]
+        avg_distance = "∞" if len(dists) == 0 else sum(dists) / len(dists)
+
+        # Occurrences 
+        occurrences = raw_data[target].get('content', '').count(article_link)
+        data[article] = (
+            avg_distance,
+            computeCommonLinks(raw_data, start, article),
+            occurrences
+        )
     with open("precomputed.bin", 'wb') as file:
         compressed_data = zlib.compress(json.dumps(data).encode('utf-8'))
         file.write(compressed_data)
@@ -164,6 +150,6 @@ def precompute(raw_data: dict, target: str):
 # calculate runtime
 start = time.time()
 raw_data, filtered, difficulty, target = load_data()
-computed_data = precompute(raw_data, target)
+computed_data = precompute(raw_data, filtered, target)
 end = time.time()
 print(f"Time taken: {end - start} seconds")
