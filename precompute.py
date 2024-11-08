63cb58075b0dfec29e8ca7df18dde1834d1e2f0d
NyxIsBad <hijirijst@gmail.com>
2024-11-07 20:29:56 -0500

    switch to only 10 for the sake of testing, cp-r in precompute.yml

--- precompute.py
+++ precompute.py
@@ -137,7 +137,7 @@ def precompute(raw_data: dict, filtered: list[str], target: str):
     from tqdm import tqdm
     with ThreadPoolExecutor(max_workers = 8) as executor:
         futures = []
-        for article in tqdm(filtered):
+        for article in tqdm(filtered[:10]):
             if article == start:
                 continue
             article_link = resolveLink(raw_data, article)
